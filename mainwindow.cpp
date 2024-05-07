783f9773306adc1f69eb0dbf829e0f0544919c71
MIO-cherno <2016530771@qq.com>
2024-05-07 21:52:47 +0800

    QtStudy day2

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -40,3 +40,19 @@ void MainWindow::contextMenuEvent(QContextMenuEvent * event)
     mExitMenu->exec(QCursor::pos());//当前鼠标位置"QCursor::pos()"传递，在此位置弹出菜单项
     event->accept();
 }
+
+void MainWindow::mousePressEvent(QMouseEvent *event)
+{
+    mOffset = event->globalPos()-this->pos();
+    //event->globalPos() 来获取鼠标指针在屏幕上的全局位置。
+    /*this->pos() 返回的是这个窗口部件在其父窗口部件中的位置（或称为“坐标”）。
+     * 这个位置也是一个 QPoint 对象，包含了窗口部件左上角的 x 和 y 坐标（相对于其父窗口部件）
+        这里时计算鼠标相对于mainwindow的坐标*/
+
+}
+
+void MainWindow::mouseMoveEvent(QMouseEvent *event)
+{
+    this->move(event->globalPos()-mOffset);
+    //减去窗口与鼠标的偏移量，否则窗口的左上角会移动到鼠标当前位置，与预期不符
+}
